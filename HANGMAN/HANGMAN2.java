76f60b47030d700febd49fe448047be44244e59a
MacDammy <makanjuolaeni555@gmail.com>
2023-02-22 18:05:48 +0100

    HANGMAN

---
+++ HANGMAN/HANGMAN2.java
@@ -0,0 +1,49 @@
+package HANGMAN;
+
+import java.util.Scanner;
+public class HANGMAN2 {
+	public void playgame() {
+		System.out.println("play game has been called!");
+	}
+	public void viewinstruction() {
+		System.out.println("view instruction has been called!");
+	}
+	public void exitgame() {
+		System.out.println("exit game has been called!");
+	}
+	public void called() {
+		Scanner sc= new Scanner(System.in);
+		System.out.print("CHOOSE AN OPTION: ");
+		int call = sc.nextInt();
+		switch (call) {
+		case 1:
+			playgame();
+		case 2:
+			viewinstruction();
+		case 3:
+			exitgame();
+		default:
+			System.err.println("INVALID INPUT");
+			
+		}
+		
+//		if ( call==3 )
+//			exitgame();
+//		else if( call==1 )
+//			playgame();
+//		else if( call==2 )
+//			viewinstruction();
+//		else
+//			System.err.println("INVALID INPUT");
+//			
+	}
+	
+	public static void main(String[] args) {
+		HANGMAN2 calll=new HANGMAN2();
+		System.out.println("1. ....Play Game.....");
+		System.out.println("2. ...View Instruction....");
+		System.out.println("3. ....Exit game.....");
+		calll.called();
+		
+	}
+}
